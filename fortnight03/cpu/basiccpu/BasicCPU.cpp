b55b4f40392c7ed79434b493e4bd5d7675f0cc35
GustavoRFS <gustavoribeiro.ribeiro1@gmail.com>
2020-12-02 01:18:47 -0300

    Finalizado o REO

--- fortnight03/cpu/basiccpu/BasicCPU.cpp
+++ fortnight03/cpu/basiccpu/BasicCPU.cpp
@@ -122,6 +122,10 @@ int BasicCPU::ID()
 		case 0x1A000000:
 			return decodeDataProcReg();
 			break;
+    case 0x0E000000:
+    case 0x1E000000:
+      return decodeDataProcFloat();
+      break;
 		
 		// TODO
 		// implementar o GRUPO A SEGUIR
@@ -343,10 +347,40 @@ int BasicCPU::decodeDataProcFloat() {
 			WBctrl = WBctrlFlag::RegWrite;
 			
 			// atribuir MemtoReg
+			MemtoReg = true;
+			
+			return 0;
+    case 0x1E202800:
+			// C7.2.43 FADD (scalar), p. 1346
+			// implementado apenas ftype='00'
+			if (IR & 0x00C00000) return 1;
+
+			fpOp = FPOpFlag::FP_REG_32;
+			
+			// ler A e B
+			n = (IR & 0x000003E0) >> 5;
+			A = getSasInt(n); // 32-bit variant
+
+			m = (IR & 0x001F0000) >> 16;
+			B = getSasInt(m);
+
+			// registrador destino
+			d = (IR & 0x0000001F);
+			Rd = &(V[d]);
+			
+			// atribuir ALUctrl
+			ALUctrl = ALUctrlFlag::ADD;
+			
+			// atribuir MEMctrl
+			MEMctrl = MEMctrlFlag::MEM_NONE;
+			
+			// atribuir WBctrl
+			WBctrl = WBctrlFlag::RegWrite;
+			
+			// atribuir MemtoReg
 			MemtoReg = false;
 			
 			return 0;
-
 		default:
 			// instrução não implementada
 			return 1;
@@ -382,6 +416,9 @@ int BasicCPU::EXI()
 		case ALUctrlFlag::SUB:
 			ALUout = A - B;
 			return 0;
+        case ALUctrlFlag::ADD:
+            ALUout= A + B;
+            return 0;
 		default:
 			// Controle não implementado
 			return 1;
@@ -422,6 +459,9 @@ int BasicCPU::EXF()
 			case ALUctrlFlag::SUB:
 				ALUout = Util::floatAsUint64Low(fA - fB);
 				return 0;
+            case ALUctrlFlag::ADD:
+                ALUout = Util::floatAsUint64Low(fA + fB);
+                return 0;
 			default:
 				// Controle não implementado
 				return 1;
